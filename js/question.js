10a05264b5eee64456a5a37adc17438bbfdca8f7
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-29 00:45:05 +0900

    add quiz js
    quiz

---
+++ js/question.js
@@ -0,0 +1,30 @@
+
+//Array of objects
+
+const quiz =[
+    {
+        q:'Which month comes right before june?',
+        option:['may', 'september','july','august'],
+        answer: 0
+    },
+    {
+        q:'CSS stands for...',
+        option:['Computer styled sections', 'Cascading style sheets','Crazy solid shapes','None of the above'],
+        answer:  0
+    },
+    {
+        q:'what color is a apple',
+        option:['red', 'yellow','white','green'],
+        answer: 0
+    },
+    {
+        q:'3 * 4 + 6 / 2 = ?',
+        option:['9', '15','21'],
+        answer: 1
+    },
+    {
+        q:'What is the capital of Japan?',
+        option:['osaka', 'kyoto','fukuoka','tokyo'],
+        answer: 3
+    }
+]
