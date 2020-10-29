82a22fb985a6bb625409fa7db2d8fdc2b21a79b4
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-29 21:47:27 +0900

    add app_js
    code fix up

---
+++ js/app.js
@@ -0,0 +1,4 @@
+
+const questionNumber = document.querySelector(",question_number");
+const questionText = document.querySelector(",question_text");
+const optionContainer = document.querySelector(",option_container");
