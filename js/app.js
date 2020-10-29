1290a43b9d0e498dcd4a405025aeed231f1396be
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-30 00:37:21 +0900

    question array
    array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 
-const questionNumber = document.querySelector(",question_number");
-const questionText = document.querySelector(",question_text");
-const optionContainer = document.querySelector(",option_container");
+const questionNumber = document.querySelector(".question_number");
+const questionText = document.querySelector(".question_text");
+const optionContainer = document.querySelector(".option_container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+
+
+//push the questions into availableQuestions Array
+function setAvailableQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i=0; i<totalQuestion; i++){
+        console.log(quiz[i])
+    }
+}
+
+window.onload = function(){
+    setAvailableQuestions();
+}
