1ab5d374aed27b5e000a1dd4e1bea9a3361c4848
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-11-04 21:52:05 +0900

    add function
    if the answer is incorrect the show the correct option by adding green color the correct option

--- js/app.js
+++ js/app.js
@@ -81,7 +81,15 @@ function getResult(element){
     }
     else{
         // set the red color to the incorrect option
-        element.classList.add("wrong")
+        element.classList.add("wrong");
+
+        //if the answer is incorrect the show the correct option by adding green color the correct option
+        const optionLen = optionContainer.children.length;
+        for(let i=0; i<optionLen; i++){
+            if(parseInt(optionContainer.children[i].id) === currentQuestion.answer){
+                optionContainer.children[i].classList.add("correct");
+            }
+        }
     }
 
     unclickableOptions();
